1ffa5937e9666ab70e2ee9ce12e805fc8622e5c1
John Duffy <Glenbard@GBN-LAB-2204-26>
2015-05-27 10:26:13 -0500

    Adds bullet collision, zombies now die when hit

--- Last Stand/src/Person.java
+++ Last Stand/src/Person.java
@@ -118,6 +118,42 @@ public class Person {
 	public void setAmmo(int ammo) {
 		this.ammo = ammo;
 	}
+	
+	public boolean hit(float[] bInfo){//bInfo is xpos, ypos, xspeed, yspeed
+		float bx=bInfo[0],by=bInfo[1],xpd=bInfo[2],ypd=bInfo[3];
+        int sx = (int)bx,sy=(int)by,ex=(int)(bx+xpd),ey=(int)(by+ypd);
+        //print("shot in your area " + commu.getAddr());
+        /*float PX = xPos,PY=yPos;
+        double opme = angleBP(sx,sy,(int)PX,(int)PY);
+        double shotang = angleBP(sx,sy,ex,ey);
+        //println(" difang " + opme + " ashot" + shotang);
+        double angle = min(min(abd(opme-shotang),abd(opme+(2*Math.PI)-shotang)),abd(opme-((2*Math.PI)+shotang)));
+        double distance = DBP((int)PX,(int)PY,sx,sy)*Math.sin((float)angle);
+        //println("distance to bullet path " + distance + " angle " + angle + " hyp " + DBP((int)PX,(int)PY,sx,sy));
+        if(distance<10){
+        	return true; 
+        }
+        return false;*/
+        if((sx>xPos && sx < xPos+size) || (ex>xPos && ex< xPos+size)){
+        	if((sy>yPos && sy < yPos+size) || (ey>yPos && ey< yPos+size)){
+            	return true;
+            }
+        }
+        return false;
+     }
+	 private double angleBP(int cx, int cy, int ex, int ey){
+	    //println("dy " + (ey-cy) + " dx " + (ex-cx) + " dy/dx " + (((float)ey-cy)/(ex-cx)));  
+	   return Math.atan2(((float)ey-cy),((float)ex-cx));
+	 }
+	 double DBP(int sx, int sy, int ex, int ey){
+		  return Math.sqrt(Math.pow(sx-ex,2)+Math.pow(sy-ey,2)); 
+	}
+	 double min(double in1, double in2){
+	  return ((in1>in2)?in2:in1); 
+	}
+		double abd(double in){
+		  return (in<0)?in*-1:in; 
+		}
 
 
 }
